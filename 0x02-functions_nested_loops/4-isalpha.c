5291f6d9ce6d65b2baa917ca276c55b2992bc19a
samierage <samierage@gmail.com>
2022-12-13 16:01:44 -0800

    Isalpha. a function that checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c: The character to be checked
  * Return: 1 if c is a later, 0 otherwise
  */
-int _isalpha(int c)
+int _Isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') ||
 	    (c >= 'A' && c <= 'Z'))
