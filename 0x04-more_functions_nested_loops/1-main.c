7862aaa71f60c41837487ca585519acd5a8e1ef7
samierage <samierage@gmail.com>
2022-12-16 00:56:55 -0800

    isdigit. a function that checks for a digit (0 through 9).

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check for code
+ *
+ * Return: Alwasy 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+	printf("%c: %d\n". c, _isdigit(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isdigi(c));
+	return (0);
+}
