d95f687054a542c969e2691493b45405c3b782f2
samierage <samierage@gmail.com>
2022-12-19 05:01:59 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function takes a pointer to an integer perameter
+ * @n: integer pointer
+ * Return: 0 Success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
