4713dfda52e8b30ef80063ab66ee2729374558ce
samierage <samierage@gmail.com>
2022-12-19 05:08:35 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef main_h
+#define main_h
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,4 +12,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif /* MAIN.H */
+#endif
