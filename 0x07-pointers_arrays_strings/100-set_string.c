5f65fed9929629e21f5c1d35dde3fc63c7e2c302
samierage <samierage@gmail.com>
2023-01-03 04:01:04 -0800

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value if a pointer to a char
+ * @s: source adress.
+ * @to: tatget adress.
+ * Return: no return.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
